fb7c3a1713136fe7055c71538416f4b30283fbd4
Sivakumaran Vithursan <sivithu93240@gmail.com>
2021-01-15 00:34:19 +0100

    fix bug store & add routes for listMenu

--- routes/menu.js
+++ routes/menu.js
@@ -7,9 +7,18 @@ var mock = require('../public/javascripts/mock')
 
 router.get('/', function(req, res, next) {
     var idMenu = req.query.idMenu;
+    var listIdMenu = req.query.listIdMenu
     if(idMenu) {
         var menuById = mock.MENU.menus.filter(menu => menu.idMenu == idMenu);
         res.json(menuById[0]);
+    } else if (listIdMenu){
+        let list = listIdMenu.split(',');
+        let listMenu = [];
+        list.forEach(id => {
+            let menu = mock.MENU.menus.filter(menu => menu.idMenu == id);
+            listMenu.push(menu[0]);
+        })
+        res.json(listMenu);
     } else {
         res.send(mock.MENU)
     }
